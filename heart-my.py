428f6c3b9a3ac4247ed115ece28699f0f3f5b711
Manishabalu <108050443+Manishabalu@users.noreply.github.com>
2022-07-23 19:00:20 +0530

    Update heart-my.py

--- heart-my.py
+++ heart-my.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('heart-my')
-data path =(https://docs.google.com/spreadsheets/d/10YpYtweIfZXjuZ-_iaAaDVFfarK8OoMptJGVfyieQMY/edit?usp=sharing)
+df=(https://docs.google.com/spreadsheets/d/10YpYtweIfZXjuZ-_iaAaDVFfarK8OoMptJGVfyieQMY/edit?usp=sharing)
 
