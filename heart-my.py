1a57f0f4bf4d099b7e316a90e1ad59dc14935448
Manishabalu <108050443+Manishabalu@users.noreply.github.com>
2022-07-23 19:14:27 +0530

    Update heart-my.py

--- heart-my.py
+++ heart-my.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.title("heart-my")
+st.title("Heart-my")
+datafile = st.file_uploader("Upload CSV",type=['csv'])
+if datafile is not None:
+   file_details = {"FileName":datafile.name,"FileType":datafile.type}
+   df  = pd.read_csv(datafile)
+   st.dataframe(df)
+   save_uploadedfile(datafile)
 
 
