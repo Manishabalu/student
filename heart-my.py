0d7edcabdb6dc4838854614a5646173fc9610309
Manishabalu <108050443+Manishabalu@users.noreply.github.com>
2022-07-23 19:04:39 +0530

    Update heart-my.py

--- heart-my.py
+++ heart-my.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title("heart-my")
-df=(https://docs.google.com/spreadsheets/d/10YpYtweIfZXjuZ-_iaAaDVFfarK8OoMptJGVfyieQMY/edit?usp=sharing)
+df=(heart_failure_clinical_records_dataset.csv)
 
