5a5c519b9e6916227290bd61495cdb5378fc61ee
Manishabalu <108050443+Manishabalu@users.noreply.github.com>
2022-07-25 18:13:30 +0530

    Update heart-my.py

--- heart-my.py
+++ heart-my.py
@@ -27,14 +27,7 @@ backgroundColor="#FFFFFF"
 secondaryBackgroundColor="#F0F4F6"
 textColor="#262990"
 font="sans serif"
-fig = px.histogram(train, x="age", color="DEATH_EVENT", marginal="violin", hover_data=train.columns, 
-                   title ="Distribution of AGE Vs DEATH_EVENT", 
-                   labels={"age": "AGE"},
-                   template="plotly",
-                   
-                   
-                  )
-fig.show()
+
 st.markdown("""
         <iframe width="600" height="600" src="https://datastudio.google.com/reporting/e0e2187f-21b5-4ebd-86b6-32fb66f40907/page/mpew" frameborder="0" style="border:0" allowfullscreen></iframe>
         """, unsafe_allow_html=True)
