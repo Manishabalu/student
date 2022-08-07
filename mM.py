7820a05aa832a07485ede41a326f7d2e8d021dcf
Manishabalu <108050443+Manishabalu@users.noreply.github.com>
2022-08-07 14:03:12 +0530

    Update mM.py

--- mM.py
+++ mM.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('MYY MOVIES')
-streamlit run MYY_MOVIES.py
+
 DATE_COLUMN = 'date/time'
 DATA_URL = ("C:\Users\MANIVASH\Downloads\credits.csv.")
 
