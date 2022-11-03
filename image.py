cf7f1e7689aeca81bb35fbd73c8eebf45e0e55dd
Manishabalu <108050443+Manishabalu@users.noreply.github.com>
2022-11-03 16:53:19 +0530

    Update image.py

--- image.py
+++ image.py
@@ -5,4 +5,4 @@ st.write("Image caption generator is a model which generates caption based on th
 st.subheader("DATASET")
 st.write("The dataset used here is the FLICKR 8K which consists of around 8091 images along with 5 captions for each images. If you have a powerful system with more than 16 GB RAM and a graphic card with more than 4 GB of memory, you can try to take FLICKR 30K which has around 30,000 images with captions.")
 st.subheader("OUTPUT")
-st.image("image caption.png")
+st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cv-foundation.org%2Fopenaccess%2Fcontent_cvpr_2015%2Fpapers%2FVinyals_Show_and_Tell_2015_CVPR_paper.pdf&psig=AOvVaw0vqkoKhl1izOYzxrM4uuou&ust=1667560928029000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCLja69rykfsCFQAAAAAdAAAAABAD")
